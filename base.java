25ef6befa4c7a4e9ad393eb4217c7151a40b241e
Eduardo Afán <eduardoafan.eae@gmail.com>
2023-12-24 18:18:43 +0100

    update

--- base.java
+++ base.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
-public class base {
+public class base{
     public static void main(String[] args) {
-    Scanner scanner =new Scanner(System.in);
-    int suma=0;
-    int resta=0;
-    System.out.println("Ingrese valor A : ");
-    int a = scanner.nextInt();
-    System.out.println("Ingrese Valor B : ");
-    int b = scanner.nextInt();
-    System.out.println("Suma: " +suma);
-    System.out.println("Resta: " +resta);
+        Scanner scanner = new Scanner(System.in);
+        int suma = 0;
+        int resta = 0;
+        int multiplicacion = 0;
+        int division = 0;
+        System.out.println("Ingrese valor A : ");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese Valor B : ");
+        int b = scanner.nextInt();
+        System.out.println("\n Resultado Suma : " + suma);
+        System.out.println("\n Resultado de la Resta : " + resta);
+        System.out.println("\n Resultado de la División : " +division);
+        System.out.println("\n Resultado de la Multiplicación : " +multiplicacion);
+        System.out.println();
 
-}}
+    }
+}
