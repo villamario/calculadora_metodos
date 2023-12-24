07dc0f5e5f27e9b1ed9199f555663940df8f570c
Mario <villamario7@gmail.com>
2023-12-24 17:58:33 +0100

    update

--- base.java
+++ base.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class base {public static void main(String[] args) {
+public class base {
+    public static void main(String[] args) {
     Scanner scanner =new Scanner(System.in);
     int suma=0;
     int resta=0;
@@ -8,7 +9,7 @@ public class base {public static void main(String[] args) {
     int a = scanner.nextInt();
     System.out.println("Ingrese Valor B : ");
     int b = scanner.nextInt();
-    System.out.println("Suma: " + suma);
+    System.out.println("Suma: " +suma);
     System.out.println("Resta: " +resta);
 
 }}
