2c2dfa1d7194830e85c6d3fd5d89d496be240e51
Eduardo Afán <148453601+Candykush777@users.noreply.github.com>
2023-12-24 17:45:22 +0100

    Add files via upload

---
+++ calculadora_metodos/metodos.java
@@ -0,0 +1,16 @@
+package calculadora_metodos;
+
+import java.util.Scanner;
+
+public class metodos{public static void main(String[] args) {
+    Scanner scanner =new Scanner(System.in);
+    int suma=0;
+    int resta=0;
+    System.out.println("Ingrese valor A : ");
+    int a = scanner.nextInt();
+    System.out.println("Ingrese Valor B : ");
+    int b = scanner.nextInt();
+    System.out.println("Suma: " + suma);
+    System.out.println("Resta: " +resta);
+
+}}
